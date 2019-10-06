4c317bc56a2d396c568772d80279b2ce829599bb
Muga Kim <ukim@ucdavis.edu>
2019-10-06 13:40:09 -0700

    added detail views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from catalog.models import Movie, Genre, MovieInstance, Actor, Language, Director
 
-admin.site.register(Movie)
+#admin.site.register(Movie)
 admin.site.register(Genre)
-admin.site.register(MovieInstance)
-admin.site.register(Actor)
+#admin.site.register(MovieInstance)
+#admin.site.register(Actor)
 admin.site.register(Language)
 admin.site.register(Director)
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'middle_name', 'date_of_birth', 'date_of_death')
+    fields = [('first_name', 'middle_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+
+class MovieInstanceInline(admin.TabularInline):
+    model = MovieInstance
+    extra = 0
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year')
+    inlines = [MovieInstanceInline]
+
+@admin.register(MovieInstance)
+class MovieInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_date')
+    fieldsets = (
+        (None, {'fields': ('movie', 'id')}),
+        ('Availability', {'fields': ('status', 'due_date')}),
+    )
--- catalog/models.py
+++ catalog/models.py
@@ -92,7 +92,7 @@ class Director(models.Model):
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'
+        return f'{self.last_name}, {self.first_name} {self.middle_name}'
 
     def get_absolute_url(self):
         return reverse('director-detail', args=[str(self.id)])
