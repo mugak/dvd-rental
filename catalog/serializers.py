d1fcc3ddbb580cd3c48eac9e0332fc63f150bec5
Muga Kim <ukim@ucdavis.edu>
2019-10-07 15:52:31 -0700

    updated endpoints

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Movie
-        fields = ['title']
+        fields = ['title', 'synopsis', 'year', 'rating']
 
 class MovieInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MovieInstance
-        fields = ['id']
+        fields = ['id', 'movie', 'due_date', 'status']
